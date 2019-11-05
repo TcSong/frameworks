492c2766d181e56d160a5c46a133f5ab7c32a259
TcSong <tcsong24@gmail.com>
2019-11-05 11:12:08 +0800

    Update floatadd.c

--- picco/source/addfloat/floatadd.c
+++ picco/source/addfloat/floatadd.c
@@ -2,8 +2,13 @@
 
 public int main()
 {
-    private float<32, 9> fa;
+    public float<32, 9> fa;
+    private float<32, 9> fb, fc;
     smcinput(fa, 1);
-    smcoutput(fa, 1);
+    smcinput(fb, 1);
+    smcinput(fc, 1);
+    public float<32, 9> rlt;
+    rlt = fa + fb + fc;
+    smcoutput(rlt, 1);
     return 0;
 }
